6f7b5eb43e21a4bc40721c7b95a932a8b1b85fd1
Wendell P. Barreto <wendellp.barreto@gmail.com>
2022-07-21 00:04:48 -0300

    chore: indentation and quotes

--- src/screens/sistema/aluno/telaTurmaListas.screen.js
+++ src/screens/sistema/aluno/telaTurmaListas.screen.js
@@ -1,81 +1,82 @@
-import React, { Component } from "react";
-import TemplateSistema from "../../../components/templates/sistema.template";
-import api, { baseUrlBackend } from "../../../services/api";
-import Row from "../../../components/ui/grid/row.component";
-import Col from "../../../components/ui/grid/col.component";
-import TurmaListasScrren from "../.././../components/screens/turmaListas.componente.screen";
+import React, { Component } from 'react'
+
+import TemplateSistema from '../../../components/templates/sistema.template'
+import Col from '../../../components/ui/grid/col.component'
+import Row from '../../../components/ui/grid/row.component'
+import api, { baseUrlBackend } from '../../../services/api'
+import TurmaListasScrren from '../.././../components/screens/turmaListas.componente.screen'
 
 export default class Listas extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       listas: [],
       loandingListas: true,
       loadingInfoTurma: true,
-      myClasses: JSON.parse(sessionStorage.getItem("myClasses")) || "",
-      turma: "",
-    };
+      myClasses: JSON.parse(sessionStorage.getItem('myClasses')) || '',
+      turma: ''
+    }
   }
 
   async componentDidMount() {
-    await this.getInfoTurma();
-    this.getListas();
+    await this.getInfoTurma()
+    this.getListas()
 
-    const { turma } = this.state;
-    document.title = `${turma && turma.name} - listas`;
+    const { turma } = this.state
+    document.title = `${turma && turma.name} - listas`
   }
   componentWillUnmount() {
-    this.io && this.io.close();
+    this.io && this.io.close()
   }
   async getInfoTurma() {
-    const id = this.props.match.params.id;
-    const { myClasses } = this.state;
-    if (myClasses && typeof myClasses === "object") {
-      const index = myClasses.map((c) => c.id).indexOf(id);
+    const id = this.props.match.params.id
+    const { myClasses } = this.state
+    if (myClasses && typeof myClasses === 'object') {
+      const index = myClasses.map(c => c.id).indexOf(id)
       if (index !== -1) {
         this.setState({
-          turma: myClasses[index],
-        });
+          turma: myClasses[index]
+        })
       }
-      this.setState({ loadingInfoTurma: false });
-      return null;
+      this.setState({ loadingInfoTurma: false })
+      return null
     }
     try {
-      const response = await api.get(`/class/${id}`);
+      const response = await api.get(`/class/${id}`)
       this.setState({
         turma: response.data,
-        loadingInfoTurma: false,
-      });
+        loadingInfoTurma: false
+      })
     } catch (err) {
-      this.setState({ loadingInfoTurma: false });
-      console.log(err);
+      this.setState({ loadingInfoTurma: false })
+      console.log(err)
     }
   }
   async getListas() {
     try {
-      const id = this.props.match.params.id;
-      let query = `?idClass=${id}`;
-      const response = await api.get(`/listQuestion${query}`);
+      const id = this.props.match.params.id
+      let query = `?idClass=${id}`
+      const response = await api.get(`/listQuestion${query}`)
       this.setState({
         listas: [...response.data],
-        loandingListas: false,
-      });
+        loandingListas: false
+      })
     } catch (err) {
-      this.setState({ loandingListas: false });
-      console.log(err);
+      this.setState({ loandingListas: false })
+      console.log(err)
     }
   }
 
   render() {
-    const { loadingInfoTurma, turma, loandingListas, listas } = this.state;
+    const { loadingInfoTurma, turma, loandingListas, listas } = this.state
     return (
-      <TemplateSistema {...this.props} active={"listas"} submenu={"telaTurmas"}>
+      <TemplateSistema {...this.props} active={'listas'} submenu={'telaTurmas'}>
         <Row mb={15}>
           <Col xs={12}>
             {loadingInfoTurma ? (
-              <div className="loader" style={{ margin: "0px auto" }}></div>
+              <div className="loader" style={{ margin: '0px auto' }}></div>
             ) : (
-              <h5 style={{ margin: "0px" }}>
+              <h5 style={{ margin: '0px' }}>
                 <i className="fa fa-users mr-2" aria-hidden="true" />
                 {turma && turma.name} - {turma && turma.year}.
                 {turma && turma.semester}
@@ -86,12 +87,12 @@ export default class Listas extends Component {
         </Row>
         {loandingListas ? (
           <Row mb={15}>
-            <div className="loader" style={{ margin: "0px auto" }}></div>
+            <div className="loader" style={{ margin: '0px auto' }}></div>
           </Row>
         ) : (
           <TurmaListasScrren {...this.state} {...this.props} listas={listas} />
         )}
       </TemplateSistema>
-    );
+    )
   }
 }
